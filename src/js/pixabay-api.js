b5a8ad924dcfbb33fc3b2fa5fe16b4516631b826
Oleksandr <gayrsuk@gmail.com>
2024-04-01 14:52:41 +0300

    for fetching

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,39 @@
-export const BASE_URL = 'https://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '43151566-329d1ba35c4912218880d8d24';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json; charset=UTF-8',
+  },
+};
+
+// export const getImages = () => {
+//   return fetch(
+//     `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent('red roses')}`,
+//     options
+//   )
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     })
+//     .then(post => console.log(post))
+//     .catch(error => console.log(error));
+// };
+
+export const getImages = () => {
+  return fetch(
+    `https://pixabay.com/api/?key=43151566-329d1ba35c4912218880d8d24&q=roses`,
+    options
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(post => console.log(post))
+    .catch(error => console.log(error));
+};
--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { getImages } from './js/pixabay-api';
+
+const searchInput = document.querySelector('input[class="search-input"]');
+const form = document.querySelector('form[class="search"]');
+
+const handleSubmit = event => {
+  event.preventDefault();
+
+  getImages();
+};
+
+form.addEventListener('submit', handleSubmit);
